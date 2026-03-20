d82343b5e74a23023a87db08da75f65da0571ab0
Chris Rohlfs <seeken@gmail.com>
2026-03-20 08:16:54 -0600

    relax selecto dependency range

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule SelectoDBSQLite.MixProject do
     if use_local_ecosystem?() do
       {:selecto, path: "../selecto"}
     else
-      {:selecto, ">= 0.4.0 and < 0.5.0"}
+      {:selecto, ">= 0.4.0 and < 0.6.0"}
     end
   end
 
